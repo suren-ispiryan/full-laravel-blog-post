95ffe4339b046266fccf4fa3c981e6693f5c9832
suren <suren.ispiryan@gmail.com>
2022-07-08 18:54:06 +0400

    installed soft delete package and made its configuration in models and migrations

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Like;
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Log;
@@ -11,7 +13,14 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, CascadeSoftDeletes, SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $cascadeDeletes = [
+        'posts'
+    ];
 
     /**
      * The attributes that are mass assignable.
